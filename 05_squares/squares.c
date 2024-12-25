b15a3c3d6fa76b93a02b1b2cce5092fb2359ffde
Coursera Learner <nobody@nowhere.nul>
2024-12-25 21:46:16 +0000

    Did 05_squares

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -22,6 +22,7 @@ int isInRange(int coord, int offset, int size) {
 int isAtBorder(int coord, int offset, int size) {
   // if coord is equal to offest or offset + size
   // return 1, else return 0
+ 
   if (coord == offset || coord == offset + size) {
     return 1;
   }
@@ -30,18 +31,24 @@ int isAtBorder(int coord, int offset, int size) {
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-  int w;
-  if (size1 > (x_offset + size2)) {
-      w = size1;
-    } else {
-      w = (x_offset + size2);
-    }
+  int w = (size1 > (x_offset + size2)) ? size1 : (x_offset + size2);
     
   //compute the max of size1 and (y_offset + size2).  Call this h
-  int h;
-  if (size1 > (y_offset + size2)) {
-    h = size1;
-  }
+  int h = (size1 > (y_offset + size2)) ? size1 : (y_offset + size2);
+
+  printf("h: %d, w: %d\n", h, w);
+  for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            if((isInRange(x, x_offset, size2) && isAtBorder(y, y_offset, size2 -1))
+               ||(isInRange(y, y_offset, size2) && isAtBorder(x, x_offset, size2 - 1))) {
+                printf("*");
+               } else if ((x < size1 && (y == 0 || y == size1-1)) || (y < size1 && (x == 0 || x == size1 - 1))) {
+                printf("#");
+               } else { printf(" "); }
+        }
+        printf("\n");
+    }
+  
   //count from 0 to h. Call the number you count with y
     //count from 0 to w. Call the number you count with x
   
@@ -64,16 +71,4 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //else print a space
     //when you finish counting x from 0 to w, 
     //print a newline
-   for (int y = 0; y < h; y++) {
-        for (int x = 0; x < w; x++) {
-            if((isInRange(x, x_offset, size2) && isAtBorder(y, y_offset, size2))
-               ||(isInRange(y, y_offset, size2) && isAtBorder(x, x_offset, size2))) {
-                printf("*");
-               } else if ((x < size1 && (y == 0 || y == size1-1)) || (y < size1 && (x == 0 || x == size1 - 1))) {
-                printf("#");
-               } else { printf(" "); }
-        }
-        printf("\n");
-    }
-  
 }
