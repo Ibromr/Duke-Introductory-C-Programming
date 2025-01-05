348608ddf47f0dc6e782441d2f36e5352a9cd168
Coursera Learner <nobody@nowhere.nul>
2025-01-05 04:13:04 +0000

    Did 07_retirement

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -11,16 +11,29 @@ void retirement(int startAge,       // in months
                 double initial,      // initial savings in dollars
                 retire_info working, // info about working
                 retire_info retired)  // info about being retired
-                {
-    printf("Age %3d month %2d you have $%.2lf\n", startAge/12, startAge%12,
-                                               initial);
+                                    {
+     double balance = initial;
+    
+    // Total number of months to run the loop (working + retired)
+    int totalMonths = working.months + retired.months;
 
-   double balance = initial; 
-   balance = balance * working.rate_of_return 
+    // Loop over all months (working + retired)
+    for (int i = 0; i < totalMonths; i++){
+        // Printing balance for the current month
+        printf("Age %3d month %2d you have $%.2lf\n", startAge / 12, startAge % 12,
+                                               balance);
+        
+        if ( i < working.months){
+            // Working period: calculate for each month
+            balance = balance + (balance * working.rate_of_return) 
                            + working.contribution;
-
-   balance = balance * retired.rate_of_return 
+        } else {
+            // Retired period for each month
+            balance = balance + (balance * retired.rate_of_return) 
                            + retired.contribution;
+        }
+        startAge++;
+    }
 }
 
 int main(void){
